c47da324ddba455eb01997cc38391eb3287221bd
George-Samir <109157277+George-Samir88@users.noreply.github.com>
2023-09-20 15:09:46 +0300

    create home view and app bar

---
+++ lib/features/home/presentation/home_view.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomeView extends StatefulWidget {
+  const HomeView({super.key});
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Icon(CupertinoIcons.home),
+        title: Text('We Chat'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.search),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.more_vert),
+          ),
+        ],
+      ),
+    );
+  }
+}
