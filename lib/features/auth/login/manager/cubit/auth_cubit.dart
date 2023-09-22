e0558fa8af4311cdf830da12bc2f24ea2765195b
George-Samir <109157277+George-Samir88@users.noreply.github.com>
2023-09-22 22:11:51 +0300

    implement signInWithGoogle method in cubit

--- lib/features/auth/login/manager/cubit/auth_cubit.dart
+++ lib/features/auth/login/manager/cubit/auth_cubit.dart
@@ -1,10 +1,19 @@
-
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:we_chat/features/auth/login/manager/auth_repo/auth_repo.dart';
 
 import 'auth_state.dart';
 
 class AuthCubit extends Cubit<AuthState> {
-  AuthCubit() : super(AuthInitial());
+  AuthCubit({required this.authRepo}) : super(AuthInitial());
+  final AuthRepo authRepo;
 
-
+  Future<void> signInWithGoogle() async {
+    emit(AuthLoadingState());
+    var result = await authRepo.signInWithGoogle();
+    result.fold((failure) {
+      emit(AuthErrorState(error: failure.toString()));
+    }, (userCredential) {
+      emit(AuthSuccessState(userCredential: userCredential));
+    });
+  }
 }
