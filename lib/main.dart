79bbd29e1e137780fb027078ab10ce19e3efb6f3
George-Samir <109157277+George-Samir88@users.noreply.github.com>
2023-09-22 22:13:39 +0300

    provide bloc to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:we_chat/features/auth/login/manager/auth_repo/auth_repo_imp.dart';
+import 'package:we_chat/features/auth/login/manager/cubit/auth_cubit.dart';
 import 'package:we_chat/features/splash/presentation/splash_view.dart';
 
 import 'firebase_options.dart';
@@ -25,25 +28,28 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'We Chat',
-      theme: ThemeData(
-        fontFamily: 'Poppins',
-        appBarTheme: AppBarTheme(
-          backgroundColor: Colors.white,
-          centerTitle: true,
-          elevation: 1,
-          iconTheme: IconThemeData(
-            color: Colors.black,
-          ),
-          titleTextStyle: TextStyle(
-              fontSize: 20.0,
+    return BlocProvider(
+      create: (context) => AuthCubit(authRepo: AuthRepoImp()),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'We Chat',
+        theme: ThemeData(
+          fontFamily: 'Poppins',
+          appBarTheme: AppBarTheme(
+            backgroundColor: Colors.white,
+            centerTitle: true,
+            elevation: 1,
+            iconTheme: IconThemeData(
               color: Colors.black,
-              fontWeight: FontWeight.normal),
+            ),
+            titleTextStyle: TextStyle(
+                fontSize: 20.0,
+                color: Colors.black,
+                fontWeight: FontWeight.normal),
+          ),
         ),
+        home: SplashView(),
       ),
-      home: SplashView(),
     );
   }
 }
