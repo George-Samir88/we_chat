5d9ac44f8ca2249c7e135825163058b53a5e79f1
George-Samir <109157277+George-Samir88@users.noreply.github.com>
2023-09-22 21:57:27 +0300

    implement method of google sign in

--- lib/features/auth/login/manager/auth_repo/auth_repo_imp.dart
+++ lib/features/auth/login/manager/auth_repo/auth_repo_imp.dart
@@ -1,11 +1,30 @@
 import 'package:dartz/dartz.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:we_chat/features/auth/login/manager/auth_repo/auth_repo.dart';
 
-class AuthRepoImp implements AuthRepo{
+class AuthRepoImp implements AuthRepo {
   @override
-  Future<Either<String, UserCredential>> signInWithGoogle() {
+  Future<Either<String, UserCredential>> signInWithGoogle() async {
+    try {
+      // Trigger the authentication flow
+      final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
 
+      // Obtain the auth details from the request
+      final GoogleSignInAuthentication? googleAuth =
+          await googleUser?.authentication;
+
+      // Create a new credential
+      final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth?.accessToken,
+        idToken: googleAuth?.idToken,
+      );
+
+      // Once signed in, return the UserCredential
+      return right(
+          await FirebaseAuth.instance.signInWithCredential(credential));
+    } catch (err) {
+      return left(err.toString());
+    }
   }
-
 }
