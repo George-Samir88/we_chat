fec2736894c484d01641f15659b966b206f1276d
George-Samir <109157277+George-Samir88@users.noreply.github.com>
2023-09-29 22:02:12 +0300

    add firebase storage

--- lib/core/global_var.dart
+++ lib/core/global_var.dart
@@ -1,8 +1,10 @@
 import 'dart:ui';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:we_chat/features/home/manager/models/user_model.dart';
 
 late Size screenSize;
 FirebaseFirestore firestore = FirebaseFirestore.instance;
+FirebaseStorage firebaseStorage = FirebaseStorage.instance;
 late ChatUser me;
